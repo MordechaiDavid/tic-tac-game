0362f3b2d2b37167ac9609ba6fa3d5ecb720d7e4
Yehuda <moti208227@gmail.com>
2022-06-14 21:50:16 +0300

    Initial commit

---
+++ src/GameActionListiner.java
@@ -0,0 +1,36 @@
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class GameActionListiner implements ActionListener {
+    TicTac ticTac;
+    public GameActionListiner(TicTac ticTac){
+        this.ticTac = ticTac;
+    }
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        for (int i= 0; i<9; i++){
+            if (e.getSource()== ticTac.buttons[i]){
+                if (ticTac.firstTurn){
+                    if (ticTac.buttons[i].getText()== ""){
+                        ticTac.buttons[i].setForeground(Color.BLUE);
+                        ticTac.buttons[i].setText("X");
+                        ticTac.firstTurn = false;
+                        ticTac.text.setText("O turn");
+                        ticTac.check();
+                    }
+                }
+                else {
+                    if (ticTac.buttons[i].getText()== ""){
+                        ticTac.buttons[i].setForeground(Color.RED);
+                        ticTac.buttons[i].setText("O");
+                        ticTac.firstTurn = true;
+                        ticTac.text.setText("X turn");
+                        ticTac.check();
+                    }
+
+                }
+            }
+        }
+    }
+}
